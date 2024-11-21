a12a81536931bee9b067ca1f10b0c126235e4a45
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2024-11-22 01:26:22 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,13 +7,55 @@ $(document).ready(function(){
 	// });
 
 	// Banner Carousel
-	// $('.banner_carousel').owlCarousel({
-	// 	items:1,
-	// 	loop:true,
-	// 	nav:true,
-	// 	dots:true,
-	// 	autoplay:true,
-	// });
+	$('.game_carousel').owlCarousel({
+		items:4.5,
+		loop:true,
+		nav:false,
+		dots:false,
+		autoplay:true,
+		margin: 0,
+		responsive:{
+			0:{
+				items:2.5, 
+			},
+			400:{
+				items:2.5, 
+			},
+			600:{
+				items:3.5,  
+			},
+			1000:{
+				items:4.5,
+			}
+		}
+	});
+
+	$('.live_carousel_main').owlCarousel({
+		items: 5,
+		loop:true,
+		nav:true,
+		dots:false,
+		autoplay:true,
+		navText: ["<img src=''>","<img src='assets/images/live/arrow.png'>"],
+		margin: 20,
+		responsive:{
+			0:{
+				items:2, 
+			},
+			400:{
+				items:3, 
+			},
+			600:{
+				items:4, 
+				margin: 5,
+			},
+			1000:{
+				items:5,
+				nav:true,
+				loop:false
+			}
+		}
+	});
 
 
 
